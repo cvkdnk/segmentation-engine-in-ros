43357daeed10c5819c58d6b1d76d7ff5ec51f994
cvkdnk <cvkdnk@outlook.com>
2023-07-24 14:55:55 +0800

    optimize read_rosbag

--- scripts/read_rosbag.py
+++ scripts/read_rosbag.py
@@ -1,11 +1,10 @@
+import argparse
 import rosbag
 from sensor_msgs.msg import PointCloud2, PointField
 import sensor_msgs.point_cloud2 as pc2
 import logging
 import numpy as np
 from tqdm import tqdm
-
-
 '''
 Topic: /velodyne_points
 Type: sensor_msgs/PointCloud2
@@ -14,40 +13,69 @@ Connections: 1
 Frequency: 9.91
 '''
 
+# e.g. python read_rosbag.py -s
+
+# CHANGE IT
+save_path = "/root/autodl-tmp/sequences/11/velodyne/"
+bag_path = "/root/autodl-tmp/f1_2023-07-05-17-01-17.bag"
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
-bag = rosbag.Bag('/root/autodl-tmp/f1_2023-07-05-17-01-17.bag')
 
-rosbag_info = bag.get_type_and_topic_info()
+def showTopicInfo(bag):
+    rosbag_info = bag.get_type_and_topic_info()
 
-# Add an empty line after import statements
-for i, (topic, topic_info) in enumerate(rosbag_info.topics.items()):
-   # Add a number for each topic
-   logging.info(f"{i+1}.")
-   # Use f-string to format strings
-   logging.info(f"Topic: {topic}")
-   logging.info(f"Type: {topic_info.msg_type}")
-   logging.info(f"Message count: {topic_info.message_count}")
-   logging.info(f"Connections: {topic_info.connections}")
-   # Use round function to keep two decimal places
-   logging.info(f"Frequency: {round(topic_info.frequency, 2)}")
-   logging.info("")
-
-for i, (topic, msg, t) in tqdm(enumerate(bag.read_messages(topics=["/velodyne_points"]))):
-    # logging.debug(f"msg type {type(msg)}")
-    cloud_points = pc2.read_points(msg, field_names=["x","y","z", "intensity"], skip_nans=True)
-    points_array = np.array(list(cloud_points))
-    points_array = points_array.astype(np.float32)
-    print("===================================================================================")
-    print(f"intensity: {points_array.max(axis=0)} {points_array.min(axis=0)} {points_array.mean(axis=0)}")
-    assert points_array.dtype == np.float32
-    points_array[:, 3] = points_array[:, 3] / 256.0
-    points_array.tofile(f"/root/autodl-tmp/sequences/11/velodyne/{i}.bin")
+    for i, (topic, topic_info) in enumerate(rosbag_info.topics.items()):
+        # Add a number for each topic
+        logging.info(f"{i+1}.")
+        # Use f-string to format strings
+        logging.info(f"Topic: {topic}")
+        logging.info(f"Type: {topic_info.msg_type}")
+        logging.info(f"Message count: {topic_info.message_count}")
+        logging.info(f"Connections: {topic_info.connections}")
+        # Use round function to keep two decimal places
+        logging.info(f"Frequency: {round(topic_info.frequency, 2)}\n")
 
 
-bag.close()
+def loadVelodynePoints(bag, save_path=None):
+    for i, (topic, msg, t) in tqdm(
+            enumerate(bag.read_messages(topics=["/velodyne_points"]))):
+        # logging.debug(f"msg type {type(msg)}")
+        cloud_points = pc2.read_points(
+            msg, field_names=["x", "y", "z", "intensity"], skip_nans=True)
+        points_array = np.array(list(cloud_points))
+        points_array = points_array.astype(np.float32)
+        # print("="*80)
+        # print(f"intensity: {points_array.max(axis=0)} {points_array.min(axis=0)} {points_array.mean(axis=0)}")
+        assert points_array.dtype == np.float32
+        points_array[:, 3] /= 255.0
 
+        if save_path is not None:
+            points_array.tofile(save_path + "{i}.bin")
+        # yield points_array
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='read_rosbag')
+    parser.add_argument('-s',
+                        '--show',
+                        action='store_true',
+                        help="show the ros topic information")
+    parser.add_argument('-x',
+                        '--xload',
+                        action="store_true",
+                        help="dont load and save the velodyne points")
+    args = parser.parse_args()
+
+    bag = rosbag.Bag(bag_path)
+
+    if args.show:
+        showTopicInfo(bag)
+
+    if not args.xload:
+        loadVelodynePoints(bag, save_path)
+
+    bag.close()
